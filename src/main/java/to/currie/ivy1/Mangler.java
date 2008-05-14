fc853e4845ebbad11dc3b5c7f1a16482a7c919a1
Hamish Currie <hamish@athelas.(none)>
2008-05-14 23:43:55 +1000

    passes checkstyle - need to decide if we modify the checks that we enforce. For some examples, see: http://www.mobilefish.com/developer/checkstyle/checkstyle_quickguide_configuration_file.html

--- src/main/java/to/currie/ivy1/Mangler.java
+++ src/main/java/to/currie/ivy1/Mangler.java
@@ -2,10 +2,20 @@ package to.currie.ivy1;
 
 import org.apache.commons.lang.WordUtils;
 
+/**
+ * Mangles strings for display.
+ *
+ * @author hamish
+ *
+ */
 public class Mangler {
 
-	public String mangle(String message)
-	{
-		return WordUtils.capitalizeFully(message);
-	}
+    /**
+     * Mangle a String.
+     * @param message the String to be mangled.
+     * @return the String
+     */
+    public final String mangle(final String message) {
+        return WordUtils.capitalizeFully(message);
+    }
 }
