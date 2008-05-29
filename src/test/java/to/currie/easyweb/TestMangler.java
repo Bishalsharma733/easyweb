193ca24959cbf6995585dc1d50faeff61d52c656
Hamish Currie <hamish@athelas.(none)>
2008-05-29 22:07:21 +1000

    moved the acceptance tests to the correct package removed tabs from tests added checkstyle rules for tests

--- src/test/java/to/currie/easyweb/TestMangler.java
+++ src/test/java/to/currie/easyweb/TestMangler.java
@@ -4,14 +4,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import to.currie.easyweb.Mangler;
-
 public class TestMangler {
 
-	@Test
-	public void testMangle() {
-		Mangler m = new Mangler();
-		String s = m.mangle("hi there");
-		assertEquals("Hi There", s);
-	}
+    @Test
+    public void testMangle() {
+        Mangler m = new Mangler();
+        String s = m.mangle("hi there");
+        assertEquals("Hi There", s);
+    }
 }
